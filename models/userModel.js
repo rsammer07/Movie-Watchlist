7a702a1c64f31907d99f7c1fe2415f77b277c850
rsammer07 <ryan.sammer077@gmail.com>
2023-10-14 16:17:09 -0700

    added userModel and movieModel

---
+++ models/userModel.js
@@ -0,0 +1,7 @@
+const mongoose = require(`mongoose`)
+
+const User = new mongoose.Schema({
+    userName: {type: String, require: true},
+    email: {type: String, require: true, unique: true}, 
+    password: {type: String, require: true}
+})
