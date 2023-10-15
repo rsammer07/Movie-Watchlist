61f38120077a7ee51a330029655677e33c7fd6aa
liethdallal <139842069+liethdallal@users.noreply.github.com>
2023-10-15 18:39:53 -0500

    test

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const express = require(`express`)
-const mongoose = require(`mongoose`)
+
+const express = require("express")
 const app = express()
 
+const port = 8000
 
-
-
-
-
-
-
-app.listen(`8080`, () => {
-    console.log(`listening!`)
+app.listen(port, (req, res) =>{
+    console.log(`Server is running on port ${port} ✅`)
 })
+
+
+app.get('/', (req, res) =>{
+    res.send("All linked up")
+})
+
+
