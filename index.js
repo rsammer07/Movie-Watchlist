068ec7c9f33510e9c20fd2834168a3c38d026d63
liethdallal <139842069+liethdallal@users.noreply.github.com>
2023-10-16 13:49:27 -0500

    Added views folder with homepage and style sheet

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const app = express()
 
 const port = 8000
 
+
+
+app.use(express.static(__dirname + '/views'));
+//We are looking in to the entire folder views to be able to grab the css along with our html
+
+app.set("view engine", "ejs")
+
+app.get('/', (req, res) => {
+    res.render('homepage.ejs')
+})
+
 app.listen(port, (req, res) =>{
     console.log(`Server is running on port ${port} ✅`)
 })
-
-
-app.get('/', (req, res) =>{
-    res.send("All linked up")
-})
-
 
