3793e6e8e3ea176920d33655fcc6ad911a5baea0
liethdallal <139842069+liethdallal@users.noreply.github.com>
2023-10-17 21:14:38 -0500

    Created all ejs files and started building out our application tempelate

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ app.set("view engine", "ejs")
 app.get('/', (req, res) => {
     res.render('homepage.ejs')
 })
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+app.get('/profile', (req, res) => {
+    res.render('profile.ejs')
+})
+app.get('/createaccount', (req, res) => {
+    res.render('createaccount.ejs')
+})
+app.get('/review', (req, res) => {
+    res.render('review.ejs')
+})
+app.get('/error', (req, res) => {
+    res.render('error.ejs')
+})
 
 app.listen(PORT, (req, res) =>{
     console.log(`Server is running on port ${PORT} ✅`)
