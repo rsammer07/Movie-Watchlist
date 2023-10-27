5b73670577f06fdf93fcbb704230aa97959b7f04
liethdallal <139842069+liethdallal@users.noreply.github.com>
2023-10-26 20:20:33 -0500

    Big changes

--- controllers/indexcontroller.js
+++ controllers/indexcontroller.js
@@ -21,10 +21,16 @@ router.get('/oauth2callback', passport.authenticate(
 ));
 
 // OAuth logout route
-router.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
-
+router.get('/logout', function(req, res) {
+    req.logout(function(err) {
+      if (err) {
+        // Handle the error, e.g., show an error message or redirect to an error page.
+        res.redirect('/error');
+      } else {
+        // Redirect the user to a different page after logging out, e.g., the home page.
+        res.redirect('/');
+      }
+    });
+  });
 
 module.exports = router;
--- controllers/moviecontroller.js
+++ controllers/moviecontroller.js
@@ -26,38 +26,44 @@ router.get("/", async (req, res, next) => {
         next(error)
     }
 })
+
+//send new movie form
+router.get("/newMovie", async(req, res, next) => {
+    try {
+        res.render("newMovieForm")
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+//send update movie form
+router.get("/update", async(req, res, next) => {
+    try {
+        res.render("movies")
+    } catch (error) {
+        next(error)
+    }
+})
+
+
 //get movies by title "movie", { movie: movie[0] }
-router.get("/:id", async (req, res, next) => {
+router.get("/:title", async (req, res, next) => {
     try {
         console.log(req)
         const title = req.params.title;
         const movie = await Movie.findOne({ title: title });
-        res.render("movie", { movie })
+        res.render("example", { movie })
     } catch (error) {
         next(error);
     }
 });
 
-//send new movie form
-router.get("/newMovie", async(req, res, next) => {
-    try {
-        res.render("/newmovie")
-    } catch (error) {
-        next(error)
-    }
-})
 
-//send update movie form
-router.get("/update", async(req, res, next) => {
-    try {
-        res.render("/updatemovie")
-    } catch (error) {
-        next(error)
-    }
-})
 
 //post new movies
-router.post("/", isLoggedIn, async(req, res, next) => {
+router.post("/newMovie", async(req, res, next) => {
+
     try {
         const createdMovie = await Movie.create({
             title: req.body.title,
@@ -65,7 +71,7 @@ router.post("/", isLoggedIn, async(req, res, next) => {
             watched: req.body.watched,
             rating: req.body.rating
         })
-        res.render("movie", { createdMovie })
+        res.redirect("/movies")
         // res.json(createdMovie)
     } catch (error) {
         next(error)
